27486ee56a1eda01826e8b346ee77f0ad747294a
zhangshaohai <>
2022-04-06 17:00:01 +0800

    初始化数据库

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func main() {
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
 	config.InitConfig(env)
+	// 初始化 DB
+	bootstrap.SetupDB()
 	r := gin.New()
 	bootstrap.SetupRoute(r)
 	err := r.Run(":" + config.Get("app.port"))
